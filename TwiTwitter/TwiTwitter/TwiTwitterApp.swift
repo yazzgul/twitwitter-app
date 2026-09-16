76d373ac68316e84328e802e8903db04524fa667
yazzgul <yazgul.khasanshina@gmail.com>
2026-09-16 13:38:56 +0300

    create ios app

---
+++ TwiTwitter/TwiTwitter/TwiTwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwiTwitterApp.swift
+//  TwiTwitter
+//
+//  Created by Язгуль Хасаншина on 16.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct TwiTwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
